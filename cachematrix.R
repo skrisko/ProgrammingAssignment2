e74b899bc309f2e7bf0bcdb64b02b2833669500f
skrisko <44063030+skrisko@users.noreply.github.com>
2018-10-29 11:05:41 +0100

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
+## I have, take the same structure as the example of makeVectore and cachemean just adapt it to the matrix case. The function solve was use
+## to find the invers. It was supposed that the invers of the matrix exist that det(x) different of 0
 
-## Write a short comment describing this function
+
+## The first function create a "special" matrix it create the matrix, set the value of it, get the value, set the value of the invers 
+## and then get it.
+
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinvers <- function(solve) m <<- invers
+  getinvers <- function() m
+  list(set = set, get = get,
+       setinvers = setinvers,
+       getinvers = getinvers)
 }
 
 
-## Write a short comment describing this function
+## The second function check if the invers was already calculated and if it the case get this value. Otherwise it calcule the value of the 
+## invers. 
 
 cacheSolve <- function(x, ...) {
+          m <- x$getinvers()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinvers(m)
+  m
         ## Return a matrix that is the inverse of 'x'
 }
